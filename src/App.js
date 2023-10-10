ba3bc17eb867597f232ebbfafcbabe31483a9952
kono-meowmeow <aya.ayatsuki@gmail.com>
2023-10-10 23:57:57 +0900

    styled-jsx

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState, useCallback, useMemo } from "react";
 import { ChildArea } from "./ChildArea";
 import { CssModules } from "./components/CssModules";
 import { InlineStyle } from "./components/InlineStyle";
+import { StyledJsx } from "./components/StyledJsx";
 import "./styles.css";
 
 export default function App() {
@@ -43,6 +44,8 @@ export default function App() {
       <InlineStyle />
       <br />
       <CssModules />
+      <br />
+      <StyledJsx />
     </div>
   );
 }
