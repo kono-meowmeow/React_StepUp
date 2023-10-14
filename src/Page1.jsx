f394398996dc1f17fe2b616f1d3db8fe37f860b9
kono-meowmeow <aya.ayatsuki@gmail.com>
2023-10-14 23:53:26 +0900

    stateを渡すページ遷移

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
 
 export const Page1 = () => {
+  const arr = [...Array(100).keys()];
+  console.log(arr);
   return (
     <>
       <div>
         <h1>Page1です</h1>
-        <Link to="/page1/detailA">DetailA</Link>
+        {/* 以下は、v6での書き方 */}
+        {/* 下記のようにstateを渡すページ遷移をすると、一覧ページでstateを取得して、詳細ページでstateを再取得するようなことを回避できる */}
+        <Link to="/page1/detailA" state={arr}>DetailA</Link>
         <br />
         <Link to="/page1/detailB">DetailB</Link>
       </div>
--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,7 +1,14 @@
+import { useLocation } from "react-router-dom";
+
 export const Page1DetailA = () => {
+  const { state } = useLocation();
+  console.log(state);
+
   return (
     <div>
       <h1>Page1DetailAです</h1>
+      {/* useLocation()で取得したLocationのstateを表示する */}
+      <p>{state}</p>
     </div>
   );
 };
